c5d9afbb1d6dbde5f76d0db251f73a2618692fb3
Crowdbotics <team@crowdbotics.com>
2020-08-06 01:17:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/metro_go_19413/MainActivity.java
@@ -0,0 +1,15 @@
+package com.metro_go_19413;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "metro_go_19413";
+    }
+}
