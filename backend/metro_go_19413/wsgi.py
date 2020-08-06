cbbf521f69ceaa60121c4bdf10cdf64cd453474e
Crowdbotics <team@crowdbotics.com>
2020-08-06 01:18:57 +0000

    Update models

--- backend/metro_go_19413/wsgi.py
+++ backend/metro_go_19413/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metro_go_19413.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "metro_go_19413.settings")
 
 application = get_wsgi_application()
